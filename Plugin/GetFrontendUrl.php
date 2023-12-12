fb4833b4676cd2e4a9173061f66676c812a77b24
konrad.konieczny <konrad.konieczny@solteq.com>
2023-12-12 10:25:08 +0100

    refactoring

--- Plugin/GetFrontendUrl.php
+++ Plugin/GetFrontendUrl.php
@@ -7,24 +7,34 @@ use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Quote\Model\QuoteIdMaskFactory;
 use Magento\Sales\Model\Order;
 use Magento\Store\Model\StoreManagerInterface;
+use Magento\Support\Model\Report\Group\Modules\Modules;
 use Paytrail\PaymentService\Controller\Receipt\Index;
 
 class GetFrontendUrl
 {
+    /**
+     * @param QuoteIdMaskFactory $idMaskFactory
+     * @param Modules $modules
+     * @param ScopeConfigInterface $config
+     * @param StoreManagerInterface $storeManager
+     */
     public function __construct(
-        private readonly QuoteIdMaskFactory $idMaskFactory,
-        private readonly \Magento\Support\Model\Report\Group\Modules\Modules $modules,
-        private readonly ScopeConfigInterface $config,
+        private readonly QuoteIdMaskFactory    $idMaskFactory,
+        private readonly Modules               $modules,
+        private readonly ScopeConfigInterface  $config,
         private readonly StoreManagerInterface $storeManager
     ) {
     }
 
     /**
+     *  Get success url based on pwa configuration.
+     *
      * @param Index $subject
      * @param string $result
      * @param Order $order
      *
      * @return string
+     * @throws NoSuchEntityException
      */
     public function afterGetSuccessUrl(Index $subject, string $result, Order $order): string
     {
@@ -38,14 +48,15 @@ class GetFrontendUrl
         }
 
         $maskedId = $this->getMaskedId($order);
-        $result   = trim($frontendBaseUrl, '/') . '/checkout/success/' . $order->getIncrementId(
-            ) . '/maskedId/' . $maskedId;
+        $result   = trim($frontendBaseUrl, '/') . '/checkout/success/' . $order->getIncrementId()
+            . '/maskedId/' . $maskedId;
 
         return $result;
     }
 
-
     /**
+     * Get cart url based on pwa configuration.
+     *
      * @param Index $subject
      * @param string $result
      * @param Order $order
@@ -72,6 +83,8 @@ class GetFrontendUrl
     }
 
     /**
+     * Get quote masked id.
+     *
      * @param Order $order
      *
      * @return string
@@ -84,6 +97,8 @@ class GetFrontendUrl
     }
 
     /**
+     * Get base url for pwa frontend
+     *
      * @return string
      */
     private function getBaseUrl(): string
